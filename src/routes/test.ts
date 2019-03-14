1433f88bff469390971be5a7134df3c4d3c4d621
weiyuan <2272632271@qq.com>
2019-03-14 17:38:12 +0800

    test jinkins

--- src/routes/test.ts
+++ src/routes/test.ts
@@ -28,6 +28,31 @@ router.get('/get/{id}', {
   }
 });
 
+router.get('/get/v2/{id}', {
+  description: '测试get',
+  tags: ['api'],
+  validate: {
+    params: {
+      id: Joi.string().description('id')
+    }
+  },
+  plugins: {
+    'hapi-swagger': {
+      responses: {
+        200: {
+          description: 'Success'
+        },
+        400: { description: 'Bad Request' }
+      }
+    }
+  },
+  handler(req, h) {
+    const id = req.params.id;
+    logger.info('查询请求v2成功', id);
+    return id;
+  }
+});
+
 router.post('/post/{id}', {
   description: '测试post',
   tags: ['api'],
